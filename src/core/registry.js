86969c57611e65724a6eecc4fc2d13ea983a56ad
MATEII96 <matei.cupes@gmail.com>
2026-06-28 12:04:06 +0300

    started codding

---
+++ src/core/registry.js
@@ -0,0 +1,4 @@
+import { DoublePendulum } from '../simulations/double-pendulum.js';
+import { WaveInterference} from '../simulations//wave-interference.js';
+import { QuantomBox } from '../simulations/quantum-box.js';
+import { KineticGas } from '../simulations/kinetic-gas.js';
